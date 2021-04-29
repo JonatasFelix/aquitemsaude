d50950469f4d8ce079cd91922e1736bba470d6e6
Jônatas Felix <jonatas.felix2007@gmail.com>
2021-04-29 19:31:26 -0300

    materialize inicial finalizado

--- aplicativo/v2-materialize/js/script.js
+++ aplicativo/v2-materialize/js/script.js
@@ -2,3 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems,);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems,);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.scrollspy');
+  var instances = M.ScrollSpy.init(elems,);
+});
